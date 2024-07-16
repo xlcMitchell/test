a386500882d7543c2a020c99e882be2f1d0ec741
BIT504 <Kiwitiendatm@gmail.com>
2024-07-16 14:12:54 +1200

    experiment one commit

--- GitProject/src/gitProjectMain.java
+++ GitProject/src/gitProjectMain.java
@@ -4,6 +4,7 @@ public class gitProjectMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
       System.out.println("Hello world");
+      System.out.println("Experiment One");
 	}
 
 }
